ca8ef7c024f35f22219ac42f72611962a0ff6af6
Andres Lora <jalc.andres.lora@gmail.com>
2023-08-20 17:58:50 -0700

    Implemented doctor About component, Feedback. Created formatData utils

---
+++ frontend/src/utils/formatDate.js
@@ -0,0 +1,8 @@
+export const formatDate = (date, config) => {
+  const defaultOptions = {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  };
+  return new Date(date).toLocaleDateString("en-US", defaultOptions);
+};
