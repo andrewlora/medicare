c3f72d9c9fe5852948810a8e80725622cd8c6311
Andres Lora <jalc.andres.lora@gmail.com>
2023-08-28 14:18:52 -0700

    created SidePanel.jsx to show Ticket price, Available time slots and  book an appointment

--- frontend/src/pages/doctors/DoctorDetails.jsx
+++ frontend/src/pages/doctors/DoctorDetails.jsx
@@ -3,6 +3,7 @@ import starIcon from "../../assets/images/Star.png";
 import doctorImg from "../../assets/images/doctor-img02.png";
 import DoctorAbout from "./DoctorAbout";
 import Feedback from "./Feedback";
+import SidePanel from "./SidePanel";
 
 const DoctorDetails = () => {
   const [tab, setTab] = useState("about");
@@ -59,6 +60,9 @@ const DoctorDetails = () => {
               {tab === "about" && <DoctorAbout />}
               {tab === "feedback" && <Feedback />}
             </div>
+          </div>
+          <div>
+            <SidePanel />
           </div>
         </div>
       </div>
