1a8b99ae9c36b65d9b6ac46d778c4911df3d91a1
Andres Lora <jalc.andres.lora@gmail.com>
2023-09-22 18:00:36 -0700

    Created verifyToken file to check the jwt token(authenticate function) and implemented in user, doctor routes middlewares

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,11 +6,13 @@ import {
   updateUser,
 } from "../controllers/userController.js";
 
+import { authenticate, restrict } from "../auth/verifyToken.js";
+
 const router = express.Router();
 
-router.get("/", getUsers);
-router.get("/:id", getUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/", authenticate, restrict(["admin"]), getUsers);
+router.get("/:id", authenticate, restrict(["patient"]), getUser);
+router.put("/:id", authenticate, restrict(["admin"]), updateUser);
+router.delete("/:id", authenticate, restrict(["admin"]), deleteUser);
 
 export default router;
