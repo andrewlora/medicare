c017975ac4b995507fb2c99346330a2f51af96e9
Andres Lora <jalc.andres.lora@gmail.com>
2023-10-10 14:50:25 -0700

    Added into user routes getUserProfile and getMyAppointments functions

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
   deleteUser,
+  getMyAppointments,
   getUser,
+  getUserProfile,
   getUsers,
   updateUser,
 } from "../controllers/userController.js";
@@ -12,7 +14,14 @@ const router = express.Router();
 
 router.get("/", authenticate, restrict(["admin"]), getUsers);
 router.get("/:id", authenticate, restrict(["patient"]), getUser);
-router.put("/:id", authenticate, restrict(["admin"]), updateUser);
-router.delete("/:id", authenticate, restrict(["admin"]), deleteUser);
+router.put("/:id", authenticate, restrict(["patient"]), updateUser);
+router.delete("/:id", authenticate, restrict(["patient"]), deleteUser);
+router.get("/profile/me", authenticate, restrict(["patient"]), getUserProfile);
+router.get(
+  "/appointments/my-appointments",
+  authenticate,
+  restrict(["patient"]),
+  getMyAppointments
+);
 
 export default router;
