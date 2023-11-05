597c39f7be8d843d5da1c5186b232ef08842cdca
Andres Lora <jalc.andres.lora@gmail.com>
2023-11-05 14:25:43 -0700

    removed console.log from ProtectedRoute.jsx file

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,17 +1,11 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { authContext } from "../context/AuthContext";
-
 const ProtectedRoute = ({ children, allowedRoles }) => {
-  console.log("allowedRoles", allowedRoles);
   const { token, role } = useContext(authContext);
-  console.log("token", token);
-  console.log("role", role);
   const isAllowed = allowedRoles.includes(role);
-
   const accessibleRoute =
     token && isAllowed ? children : <Navigate to="/login" replace={true} />;
   return accessibleRoute;
 };
-
 export default ProtectedRoute;
