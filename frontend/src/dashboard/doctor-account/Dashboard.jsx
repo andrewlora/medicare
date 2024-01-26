bf236a21809b7a3e7f3f1ea53b58bcc02e04de80
Andres Lora <jalc.andres.lora@gmail.com>
2024-01-26 14:06:09 -0700

    Added Dashboard and Tabs into Doctor Account

--- frontend/src/dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/dashboard/doctor-account/Dashboard.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-
+import { useState } from "react";
+import Error from "../../components/Error/Error";
+import Loader from "../../components/Loader/Loading";
+import { BASE_URL } from "../../config";
+import useFetchData from "../../hooks/useFetchData";
+import Tabs from "./Tabs";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { loading, error } = useFetchData(`${BASE_URL}/doctor/profile/me`);
+  const [tab, setTab] = useState("overview");
+  return (
+    <section>
+      <div className="max-w-[1170px] px-5 mx-auto">
+        {loading && !error && <Loader />}
+        {error && !loading && <Error />}
+        {!loading && !error && (
+          <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
+            <Tabs tab={tab} setTab={setTab} />
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
